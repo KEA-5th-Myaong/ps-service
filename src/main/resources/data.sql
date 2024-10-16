480cfef7524d853499a394938d00dbfb7abf142e
Sujin Kang <1214sujin@naver.com>
2024-10-16 20:07:45 +0900

    :sparkles: feature: 데이터 초기화 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into "ps" (ps_id, member_id, title, position, reason, content, created_at, updated_at) values
+	(1, 5, 'Eius cupiditate a suscipit.', '전산 자료 입력원 및 사무 보조원', 'Deleniti explicabo.', 'Reprehenderit libero omnis enim id autem corporis. Veritatis unde alias. Assumenda ducimus facilis sit id.
+Sequi quisquam libero ipsam dolor molestiae.', now(), now()),
+	(2, 5, 'Reprehenderit facere assumenda minus molestias.', '기타 채굴 및 토목 관련 종사자', 'Iste debitis.', 'Repudiandae iusto earum aspernatur esse dolor. Sunt mollitia quos quaerat ratione quae a. Temporibus porro incidunt omnis maxime est sequi pariatur.', now(), now()),
+	(3, 5, 'Nemo autem minima. Asperiores aspernatur maxime.', '주차 관리원 및 안내원', 'Vero culpa ipsum.', 'Aspernatur assumenda minus. Occaecati laboriosam minus facilis soluta. Ab repellat reiciendis earum. Corporis asperiores aut iure deleniti dolor dolores.', now(), now()),
+	(4, 5, 'Provident reiciendis eum quos impedit sapiente.', '재단사', 'Impedit doloribus.', 'A ea at. Alias recusandae voluptatibus quod quisquam id eaque. Illo iste eveniet officiis fugiat architecto occaecati accusantium.', now(), now()),
+	(5, 5, 'Excepturi quod eius accusamus vitae numquam.', '한식 주방장 및 조리사', 'Officia occaecati.', 'Quis commodi ex nisi sequi voluptate ipsa. Assumenda dolor debitis illum. Cupiditate ipsam excepturi.', now(), now());
+
